0548d22c29de8e5a1f7698e9ffd6717b053be23a
Fan <fanstars1020@163.com>
2026-02-21 16:40:39 +0800

    修复变量调用

--- src/config.rs
+++ src/config.rs
@@ -56,7 +56,6 @@ lazy_static::lazy_static! {
     static ref STATUS: RwLock<Status> = RwLock::new(Status::load());
     static ref TRUSTED_DEVICES: RwLock<(Vec<TrustedDevice>, bool)> = Default::default();
     static ref ONLINE: Mutex<HashMap<String, i64>> = Default::default();
-    pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new("".to_owned());
     pub static ref PROD_RENDEZVOUS_SERVER: RwLock = RwLock::new(match option_env!("RENDEZVOUS_SERVER") {
     Some(key) if !key.is_empty() => key,
       _ => "",
